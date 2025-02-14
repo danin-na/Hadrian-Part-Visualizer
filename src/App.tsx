bd1a558bbfe03cb261b46cc31f6f727118b0e2a0
Danin <apps.danin@gmail.com>
2025-02-13 19:47:49 -0800

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ function App ()
 {
 
   return (
-    <>
-      hellow word
-      <PartLoader />
-    </>
+    <section className="w-full h-full">
+      <div className="max-w-screen-xl mx-auto px-4 bg-red-300 h-full">
+        <PartLoader />
+      </div>
+    </section>
+
+
   )
 }
 
