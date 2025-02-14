999e461e881c32992b9e667a1723f76c48be57c6
Danin Na <Danin@ket.redm>
2025-02-14 04:06:43 -0800

    Finalize

--- src/model/interface.tsx
+++ src/model/interface.tsx
@@ -1,52 +1,38 @@
-// interfaces.ts
 import * as THREE from 'three';
 
-enum EntityType
+export interface Geo 
 {
-    ENTITY_TYPE_PLANE = "ENTITY_TYPE_PLANE",
-    ENTITY_TYPE_CYLINDER = "ENTITY_TYPE_CYLINDER",
-    ENTITY_TYPE_ROTATIONAL = "ENTITY_TYPE_ROTATIONAL",
-    ENTITY_TYPE_NURBS = "ENTITY_TYPE_NURBS",
+    id: number
+    name?: string
+    geo?: THREE.BufferGeometry
 }
 
-enum EdgeType
+export interface Rgb
 {
-    EDGE_TYPE_OUTER = "EDGE_TYPE_OUTER",
-    EDGE_TYPE_INNER = "EDGE_TYPE_INNER",
+    id: number
+    rgb: string
 }
 
-interface EdgeCurve
+export interface Ent
 {
-    startPoint: number[];
-    midPoint: number[];
-    endPoint: number[];
-    startPointNormal: number[];
+    id: number
+    ent?: {
+        type: number
+        centerUv: number[]
+        centerPoint: number[]
+        centerNormal: number[]
+        area: number
+        minRadius: number
+        minPosRadius: number
+        minNegRadius: number
+        edgeCurveChains: any[]
+    }
 }
 
-interface EdgeCurveChain
+export interface Mesh
 {
-    edgeType: EdgeType;
-    edgeCurves: EdgeCurve[];
-}
-
-interface EntityGeometryInfo
-{
-    entityType: EntityType;
-    entityId: string;
-    centerUv: number[];
-    centerPoint: number[];
-    centerNormal: number[];
-    area: number;
-    minRadius: number;
-    minPosRadius: number;
-    minNegRadius: number;
-    edgeCurveChains: EdgeCurveChain[];
-}
-
-interface Model
-{
-    id: number;
-    color: string;
-    bufferGeometry: THREE.BufferGeometry;
-    entityGeometryInfo?: EntityGeometryInfo;
+    id: number
+    geo: Geo
+    ent: Ent
+    rgb: Rgb
 }
