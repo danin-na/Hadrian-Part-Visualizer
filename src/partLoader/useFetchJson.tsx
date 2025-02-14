25d9800c2a06b0c873fcd94bef8e791cdfa35fb0
Danin <apps.danin@gmail.com>
2025-02-13 19:06:28 -0800

    init useFetchJson

---
+++ src/partLoader/useFetchJson.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function useFetchJson (url: string)
+{
+    const [data, setData] = useState(null);
+
+    useEffect(() =>
+    {
+        async function fetchData ()
+        {
+            const response = await fetch(url);
+            if (response.ok) {
+                const jsonData = await response.json();
+                setData(jsonData);
+            }
+        }
+        fetchData();
+    }, [url]);
+
+    return { data };
+}
+
+export default useFetchJson;
