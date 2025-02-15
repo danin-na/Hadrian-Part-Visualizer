a270f5b32c7bb06400e8e4cadaf7a766e3d1c49f
Danin Na <Danin@ket.redm>
2025-02-14 22:38:29 -0800

    ✅ use LODASH to optimize all the calls

--- src/model/model.tsx
+++ src/model/model.tsx
@@ -1,89 +1,177 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage, useGLTF, } from '@react-three/drei';
+import { OrbitControls, Stage, useGLTF, useSelect, } from '@react-three/drei';
 import { useState, useEffect } from 'react';
-import { Geo, Rgb, Ent, Cnc, Mesh } from './interface'
-import { getRgb, getEnt, getGeo, getCnc } from './helpres'
+import * as THREE from 'three';
+
+import _ from "lodash"
+
+interface interfaceRGB
+{
+    id: string;
+    rgb: {
+        code: string;
+    }
+
+}
+
+interface interfaceGEO
+{
+    id: string
+    name: string
+    geo: THREE.BufferGeometry
+}
+
+interface interfaceENT
+{
+    id: string
+    ent: {
+        type: number
+        centerUv: number[]
+        centerPoint: number[]
+        centerNormal: number[]
+        area: number
+        minRadius: number
+        minPosRadius: number
+        minNegRadius: number
+        edgeCurveChains: any[]
+    }
+}
 
 export default function PartLoader ()
 {
-    const [geo, setGeo] = useState<Geo[]>([]);
-    const [rgb, setRgb] = useState<Rgb[]>([]);
-    const [ent, setEnt] = useState<Ent[]>([]);
-    const [cnc, setCnc] = useState<Cnc[]>([]);
-    const [meshes, setMeshes] = useState<Mesh[]>([]);
+
+    const [GEO, setGEO] = useState<interfaceGEO[]>([]);
+    const [RGB, setRGB] = useState<interfaceRGB[]>([]);
+    const [ENT, setENT] = useState<interfaceENT[]>([]);
 
 
-
+    const [MESH, setMESh] = useState<any[]>([]);
 
     const { scene } = useGLTF('./colored_glb.glb');
+
+    const useFetchGEO = async (data: THREE.Group): Promise<void> =>
+    {
+
+        const meshes: THREE.Mesh[] = [];
+        data.traverse((obj) =>
+        {
+            if ((obj as THREE.Mesh).isMesh) {
+                meshes.push(obj as THREE.Mesh);
+            }
+        });
+
+        const newGEO = _.chain(meshes)
+            .map((mesh: THREE.Mesh) =>
+            {
+                const [name, num, id] = mesh.name.split('_');
+                return {
+                    id: String(id),
+                    name: String(mesh.name),
+                    geo: mesh.geometry,
+                };
+            })
+            .value();
+
+        setGEO(newGEO);
+    };
+
+
+    const useFetchRGB = async (url: string): Promise<void> =>
+    {
+        const response = await fetch(url);
+        const data: { [key: string]: number } = await response.json();
+
+        const newRGB = _.chain(data)
+            .toPairs()
+            .map(([code, id]) =>
+            {
+                const [R, G, B] = code.split('-');
+                return {
+                    id: String(id),
+                    rgb: { code: `rgb(${R},${G},${B})` }
+                };
+            })
+            .sortBy(item => Number(item.id))
+            .value();
+
+        setRGB(newRGB);
+    };
+
+    const useFetchENT = async (url: string): Promise<void> =>
+    {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const newENT = _.chain(data)
+            //.toPairs() it already an array
+            .map((ent) =>
+            {
+                return {
+                    id: ent.entityId,
+                    ent: {
+                        type: ent.entityType,
+                        centerUv: ent.centerUv,
+                        centerPoint: ent.centerPoint,
+                        centerNormal: ent.centerNormal,
+                        area: ent.area,
+                        minRadius: ent.minRadius,
+                        minPosRadius: ent.minPosRadius,
+                        minNegRadius: ent.minNegRadius,
+                        edgeCurveChains: ent.edgeCurveChains,
+                    },
+                };
+            })
+            .sortBy(item => Number(item.id))
+            .value();
+
+        setENT(newENT);
+    };
+
+
 
     useEffect(() =>
     {
         if (!scene) return
 
-        getGeo(scene).then(G => setGeo(G));
+        useFetchGEO(scene)
+        useFetchRGB('./rgb_id_to_entity_id_map.json')
+        useFetchENT('./entity_geometry_info.json')
 
-        getCnc().then(C => setCnc(C))
-
-        getRgb().then(R => setRgb(R))
-
-        getEnt().then(E => setEnt(E))
-
-
-    }, [scene]);
+    }, [scene])
 
     useEffect(() =>
     {
+        if (RGB.length === 0 || GEO.length === 0 || ENT.length === 0) return
 
-        if (geo.length && rgb.length && ent.length && cnc.length) {
 
-            const geoMap = new Map(geo.map((g) => [g.geo.id, g]));
-            const rgbMap = new Map(rgb.map((r) => [r.rgb.id, r]));
-            const entMap = new Map(ent.map((e) => [e.ent.id, e]));
-            const cncMap = new Map(cnc.map((c) => [c.cnc.id, c]));
+        console.log("🟩 GEO : ", GEO)
+        console.log("🟥 RGB : ", RGB)
+        console.log("🟨 ENT : ", ENT)
 
-            const meshIds = new Set([
-                ...geoMap.keys(),
-                ...rgbMap.keys(),
-                ...entMap.keys(),
-                ...cncMap.keys(),
-            ]);
+        const test = _.merge(RGB, GEO, ENT)
 
-            const createdMeshes: Mesh[] = [];
 
-            meshIds.forEach((id) =>
-            {
-                const geoItem = geoMap.get(id);
-                const rgbItem = rgbMap.get(id);
-                const entItem = entMap.get(id);
-                const cncItem = cncMap.get(id);
+        setMESh(test)
 
-                if (geoItem && rgbItem && entItem && cncItem) {
-                    createdMeshes.push({
-                        id,
-                        geo: geoItem,
-                        rgb: rgbItem,
-                        ent: entItem,
-                        cnc: cncItem,
-                    });
-                }
-            });
+    }, [RGB, GEO, ENT])
 
-            console.log("Meshes", createdMeshes);
-            setMeshes(createdMeshes);
-        }
-    }, [geo, rgb, ent, cnc]);
+    useEffect(() =>
+    {
+        if (MESH.length === 0) return
+        console.log('MESH', MESH)
+
+    }, [MESH])
+
 
     return (
+
         <Canvas shadows dpr={[1, 1.5]} camera={{ position: [4, 3, 2], fov: 35 }}>
-
             <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.9} />
-
             <Stage intensity={0.1} shadows="contact" adjustCamera={3}>
                 <group>
-                    {meshes.map((meshData) => (
-                        <mesh key={meshData.id} geometry={meshData.geo.geo.source}>
-                            <meshStandardMaterial color={meshData.cnc.cnc.sss ? "lightblue" : "#333333"} />
+                    {MESH.map((meshData: any) => (
+                        <mesh key={meshData.id} geometry={meshData.geo}>
+                            <meshStandardMaterial color="#cc3333" />
                         </mesh>
                     ))}
                 </group>
