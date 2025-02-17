bdd9a9f8d114746055386f79d3c65c1418e41528
Danin <apps.danin@gmail.com>
2025-02-16 22:23:37 -0800

    Cleanup in files

---
+++ src/model/heplpers_interface.tsx
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+
+interface interfaceRGB
+{
+    id: string;
+    rgb: {
+        code: string;
+    }
+
+}
+
+interface interfaceGEO
+{
+    id: string
+    name: string
+    geo: THREE.BufferGeometry
+}
+
+interface interfaceENT
+{
+    id: string
+    ent: {
+        type: number
+        centerUv: number[]
+        centerPoint: number[]
+        centerNormal: number[]
+        area: number
+        minRadius: number
+        minPosRadius: number
+        minNegRadius: number
+        edgeCurveChains: any[]
+    }
+}
+
+interface interfaceNBR
+{
+    id: string
+    nbr:
+    {
+        neighbors: string[]
+        neighbors_number: number
+    }
+}
+
+export type {
+    interfaceENT,
+    interfaceGEO,
+    interfaceNBR,
+    interfaceRGB
+}
--- src/model/model.tsx
+++ src/model/model.tsx
@@ -1,182 +1,72 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage, useGLTF, useSelect, } from '@react-three/drei';
+import { OrbitControls, Stage, useGLTF } from '@react-three/drei';
 import { useState, useEffect } from 'react';
-import * as THREE from 'three';
+import { useFetchGEO, useFetchRGB, useFetchENT, useFetchNBR, interfaceGEO, interfaceRGB, interfaceENT, interfaceNBR } from './helpres';
 
-import _ from "lodash"
 
-interface interfaceRGB
-{
-    id: string;
-    rgb: {
-        code: string;
-    }
-
-}
-
-interface interfaceGEO
-{
-    id: string
-    name: string
-    geo: THREE.BufferGeometry
-}
-
-interface interfaceENT
-{
-    id: string
-    ent: {
-        type: number
-        centerUv: number[]
-        centerPoint: number[]
-        centerNormal: number[]
-        area: number
-        minRadius: number
-        minPosRadius: number
-        minNegRadius: number
-        edgeCurveChains: any[]
-    }
-}
 
 export default function PartLoader ()
 {
-
     const [GEO, setGEO] = useState<interfaceGEO[]>([]);
     const [RGB, setRGB] = useState<interfaceRGB[]>([]);
     const [ENT, setENT] = useState<interfaceENT[]>([]);
-
-
+    const [NBR, setNBR] = useState<interfaceNBR[]>([]);
     const [MESH, setMESh] = useState<any[]>([]);
 
     const { scene } = useGLTF('./colored_glb.glb');
 
-    const useFetchGEO = async (data: THREE.Group): Promise<void> =>
+    useEffect(() =>
     {
+        if (!scene) return;
 
-        const meshes: THREE.Mesh[] = [];
-        data.traverse((obj) =>
+        useFetchGEO(scene).then(setGEO);
+        useFetchRGB('./rgb_id_to_entity_id_map.json').then(setRGB);
+        useFetchENT('./entity_geometry_info.json').then(setENT);
+        useFetchNBR('./adjacency_graph.json').then(setNBR);
+
+    }, [scene]);
+
+    useEffect(() =>
+    {
+        if (RGB.length === 0 || GEO.length === 0 || ENT.length === 0 || NBR.length === 0) return;
+
+        const updatedMeshes = GEO.map(geoItem =>
         {
-            if ((obj as THREE.Mesh).isMesh) {
-                meshes.push(obj as THREE.Mesh);
-            }
+            const rgbData = RGB.find(item => item.id === geoItem.id)?.rgb;
+            const entData = ENT.find(item => item.id === geoItem.id)?.ent;
+            const nbrData = NBR.find(item => item.id === geoItem.id)?.nbr;
+
+            geoItem.geo.userData = {
+                ...geoItem.geo.userData,
+                id: geoItem.id,
+                rgb: rgbData,
+                ent: entData,
+                nbr: nbrData,
+            };
+
+            return geoItem;
         });
 
-        const newGEO = _.chain(meshes)
-            .map((mesh: THREE.Mesh) =>
-            {
-                const [name, num, id] = mesh.name.split('_');
-                return {
-                    id: String(id),
-                    name: String(mesh.name),
-                    geo: mesh.geometry,
-                };
-            })
-            .value();
+        setMESh(updatedMeshes);
+    }, [RGB, GEO, ENT, NBR]);
 
-        setGEO(newGEO);
+    const ss = (e: any) =>
+    {
+        console.log(e.object);
     };
-
-
-    const useFetchRGB = async (url: string): Promise<void> =>
-    {
-        const response = await fetch(url);
-        const data: { [key: string]: number } = await response.json();
-
-        const newRGB = _.chain(data)
-            .toPairs()
-            .map(([code, id]) =>
-            {
-                const [R, G, B] = code.split('-');
-                return {
-                    id: String(id),
-                    rgb: { code: `rgb(${R},${G},${B})` }
-                };
-            })
-            .sortBy(item => Number(item.id))
-            .value();
-
-        setRGB(newRGB);
-    };
-
-    const useFetchENT = async (url: string): Promise<void> =>
-    {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        const newENT = _.chain(data)
-            //.toPairs() it already an array
-            .map((ent) =>
-            {
-                return {
-                    id: ent.entityId,
-                    ent: {
-                        type: ent.entityType,
-                        centerUv: ent.centerUv,
-                        centerPoint: ent.centerPoint,
-                        centerNormal: ent.centerNormal,
-                        area: ent.area,
-                        minRadius: ent.minRadius,
-                        minPosRadius: ent.minPosRadius,
-                        minNegRadius: ent.minNegRadius,
-                        edgeCurveChains: ent.edgeCurveChains,
-                    },
-                };
-            })
-            .sortBy(item => Number(item.id))
-            .value();
-
-        setENT(newENT);
-    };
-
-
-
-    useEffect(() =>
-    {
-        if (!scene) return
-
-        useFetchGEO(scene)
-        useFetchRGB('./rgb_id_to_entity_id_map.json')
-        useFetchENT('./entity_geometry_info.json')
-
-    }, [scene])
-
-    useEffect(() =>
-    {
-        if (RGB.length === 0 || GEO.length === 0 || ENT.length === 0) return
-
-
-        console.log("🟩 GEO : ", GEO)
-        console.log("🟥 RGB : ", RGB)
-        console.log("🟨 ENT : ", ENT)
-
-        const test = _.merge(RGB, GEO, ENT)
-
-
-        setMESh(test)
-
-    }, [RGB, GEO, ENT])
-
-    useEffect(() =>
-    {
-        if (MESH.length === 0) return
-        console.log('MESH', MESH)
-
-    }, [MESH])
-
 
     return (
-
         <Canvas shadows dpr={[1, 1.5]} camera={{ position: [4, 3, 2], fov: 35 }}>
             <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.9} />
             <Stage intensity={0.1} shadows="contact" adjustCamera={3}>
                 <group>
-                    {MESH.map((meshData: any) => (
-                        <mesh key={meshData.id} geometry={meshData.geo}>
+                    {MESH.map((meshData) => (
+                        <mesh key={meshData.id} geometry={meshData.geo} onClick={ss}>
                             <meshStandardMaterial color="#cc3333" />
                         </mesh>
                     ))}
                 </group>
             </Stage>
         </Canvas>
-
     );
 }
